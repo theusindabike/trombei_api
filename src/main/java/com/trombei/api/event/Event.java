1473c71fba4e99c7251152c5dd5b90a1cb940795
Matheus Lopes <mlopes.matheus@gmail.com>
2023-08-08 16:59:28 -0300

    chore: setup and add event entity

---
+++ src/main/java/com/trombei/api/event/Event.java
@@ -0,0 +1,50 @@
+package com.trombei.api.event;
+
+
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length=50, nullable=false)
+    private String name;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date endDate;
+
+    @Column(length=255)
+    private String description;
+
+    // private List<User> owners;
+
+    @Enumerated(EnumType.STRING)
+    private EventStatus status;
+
+    @CreatedDate
+    private Date createdAt;
+
+    public Event(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/trombei/api/event/EventStatus.java
@@ -0,0 +1,6 @@
+package com.trombei.api.event;
+
+public enum EventStatus {
+    DRAFT, 
+    PUBLISHED    
+}
